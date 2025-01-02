87541895377b40041f01dbb6dacbfba3163683e6
rwqfsfasxc100 <123430291+rwqfsfasxc100@users.noreply.github.com>
2025-01-02 13:58:13 -0800

    Add files via upload

---
+++ CurrentGame.gd
@@ -0,0 +1,17 @@
+extends "res://CurrentGame.gd"
+
+
+func _init():
+	usedShipsPool = usedShipsPool + [
+		{"name":"Tsukuyuomi-Decom", "age":24 * 3600 * 365 * 200},
+		{"name":"AT225-STUB", "age":24 * 3600 * 365 * 200},
+		{"name":"ATLAS-WASP", "age":24 * 3600 * 365 * 200},
+		{"name":"COTHON-LUX", "age":24 * 3600 * 365 * 200},
+		{"name":"PIGEON-PROSPECTOR", "age":24 * 3600 * 365 * 200},
+		{"name":"MAD-CERF-CIV", "age":24 * 3600 * 365 * 200},
+		{"name":"OBERON", "age":24 * 3600 * 365 * 200},
+		{"name":"OCP209-DD", "age":24 * 3600 * 365 * 200},
+		{"name":"OCP209-SNAP", "age":24 * 3600 * 365 * 200},
+		{"name":"TRTL-OCP", "age":24 * 3600 * 365 * 200},
+		{"name":"TRTL-RAM", "age":24 * 3600 * 365 * 200}
+		]
