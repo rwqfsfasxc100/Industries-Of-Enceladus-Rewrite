4ede853867116dc743afb7349b043e127eb05db8
rwqfsfasxc100 <123430291+rwqfsfasxc100@users.noreply.github.com>
2025-05-17 08:45:20 -0700

    updated to 2.3.2

--- ModMain.gd
+++ ModMain.gd
@@ -5,10 +5,10 @@ extends Node
 const MOD_PRIORITY = -99
 # Name of the mod, used for writing to the logs
 const MOD_NAME = "IoE Compatability Rewrite"
-const MOD_VERSION = "2.3.1"
+const MOD_VERSION = "2.3.2"
 const MOD_VERSION_MAJOR = 2
 const MOD_VERSION_MINOR = 3
-const MOD_VERSION_BUGFIX = 1
+const MOD_VERSION_BUGFIX = 2
 const MOD_VERSION_METADATA = ""
 # Path of the mod folder, automatically generated on runtime
 var modPath:String = get_script().resource_path.get_base_dir() + "/"
