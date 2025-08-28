52fdfe84e92e4e66c2f9be3a2e500f8e5c5441ca
rwqfsfasxc100 <123430291+rwqfsfasxc100@users.noreply.github.com>
2025-08-27 22:28:48 -0700

    Create WEAPONSLOT_MODIFY_TEMPLATES.gd

---
+++ HEVLIB_EQUIPMENT_DRIVER_TAGS/WEAPONSLOT_MODIFY_TEMPLATES.gd
@@ -0,0 +1,20 @@
+extends Node
+
+const WEAPONSLOT_MODIFY_TEMPLATES = {
+	"CRADLES":{
+		"equipment":[
+			"SYSTEM_EXODYN-R",
+			"SYSTEM_EXODYN-L"
+		],
+		"data":[
+			{
+				"property":"position",
+				"value":"Vector2(0,16)"
+			},
+			{
+				"property":"visible",
+				"value":"false"
+			}
+		]
+	}
+}
