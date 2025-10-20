d645aec693687320f6cc041036ebcee98c5f5415
__hev <123430291+rwqfsfasxc100@users.noreply.github.com>
2025-10-19 19:15:51 -0700

    fixed Runasimi KRB-500 being able to be fitted to rear low stress hardpoints and middle low stress hardpoints

--- HEVLIB_EQUIPMENT_DRIVER_TAGS/SLOT_TAGS.gd
+++ HEVLIB_EQUIPMENT_DRIVER_TAGS/SLOT_TAGS.gd
@@ -1,5 +1,8 @@
 extends Node
 
 const SLOT_TAGS = {
-	
+	"MiddleRightWeaponSlot":{"override_subtractive":["EQUIPMENT_MANIPULATOR_LIGHT"]},
+	"MiddleLeftWeaponSlot":{"override_subtractive":["EQUIPMENT_MANIPULATOR_LIGHT"]},
+	"LeftRearSlot":{"override_subtractive":["EQUIPMENT_MANIPULATOR_LIGHT"]},
+	"RightRearSlot":{"override_subtractive":["EQUIPMENT_MANIPULATOR_LIGHT"]},
 }
