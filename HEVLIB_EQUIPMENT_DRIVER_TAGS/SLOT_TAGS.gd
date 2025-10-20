9fb3073883abe62b8b2adfc68cfaee3d955a5cad
rwqfsfasxc100 <123430291+rwqfsfasxc100@users.noreply.github.com>
2025-10-20 10:19:40 -0700

    reverted removal of the KRB from middle low stress hardpoint

--- HEVLIB_EQUIPMENT_DRIVER_TAGS/SLOT_TAGS.gd
+++ HEVLIB_EQUIPMENT_DRIVER_TAGS/SLOT_TAGS.gd
@@ -1,8 +1,8 @@
 extends Node
 
 const SLOT_TAGS = {
-	"MiddleRightWeaponSlot":{"override_subtractive":["EQUIPMENT_MANIPULATOR_LIGHT"]},
-	"MiddleLeftWeaponSlot":{"override_subtractive":["EQUIPMENT_MANIPULATOR_LIGHT"]},
+#	"MiddleRightWeaponSlot":{"override_subtractive":["EQUIPMENT_MANIPULATOR_LIGHT"]},
+#	"MiddleLeftWeaponSlot":{"override_subtractive":["EQUIPMENT_MANIPULATOR_LIGHT"]},
 	"LeftRearSlot":{"override_subtractive":["EQUIPMENT_MANIPULATOR_LIGHT"]},
 	"RightRearSlot":{"override_subtractive":["EQUIPMENT_MANIPULATOR_LIGHT"]},
 }
