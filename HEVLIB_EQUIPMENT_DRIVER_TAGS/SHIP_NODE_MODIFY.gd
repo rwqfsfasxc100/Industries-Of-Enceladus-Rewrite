8d961d693dc3982ca0ccff4a6f84fd0ad3c5d76a
__hev <123430291+rwqfsfasxc100@users.noreply.github.com>
2025-11-09 17:34:19 -0800

    added node modifications

---
+++ HEVLIB_EQUIPMENT_DRIVER_TAGS/SHIP_NODE_MODIFY.gd
@@ -0,0 +1,110 @@
+extends Node
+
+const SHIP_TRTL = {
+	"ship_name":"SHIP_TRTL",
+	"modifications":[
+		{
+			"path":"CargoBaffles",
+			"property":"slot",
+			"recurse_to_variants":true,
+			"value":"cargo.modifierDivided"
+		
+		}
+	]
+}
+
+const SHIP_EIME = {
+	"ship_name":"SHIP_EIME",
+	"modifications":[
+		{
+			"path":"CargoBaffle",
+			"property":"slot",
+			"recurse_to_variants":true,
+			"value":"cargo.modifierDivided"
+		}
+	]
+}
+
+const SHIP_PROSPECTOR_BALD = {
+	"ship_name":"SHIP_PROSPECTOR_BALD",
+	"modifications":[
+		{
+			"path":"CargoBaffles",
+			"property":"slot",
+			"recurse_to_variants":true,
+			"value":"cargo.modifierDivided"
+		}
+	]
+}
+
+const SHIP_PROSPECTOR = {
+	"ship_name":"SHIP_PROSPECTOR",
+	"modifications":[
+		{
+			"path":"CargoBaffles",
+			"property":"slot",
+			"recurse_to_variants":true,
+			"value":"cargo.modifierDivided"
+		}
+	]
+}
+
+const SHIP_COTHON_V = {
+	"ship_name":"SHIP_COTHON_V",
+	"modifications":[
+		{
+			"path":"CargoBaffles",
+			"property":"slot",
+			"recurse_to_variants":true,
+			"value":"cargo.modifierAmorphic"
+		}
+	]
+}
+
+const SHIP_COTHON = {
+	"ship_name":"SHIP_COTHON",
+	"modifications":[
+		{
+			"path":"CargoBaffles",
+			"property":"slot",
+			"recurse_to_variants":true,
+			"value":"cargo.modifierDivided"
+		}
+	]
+}
+
+const SHIP_AT225 = {
+	"ship_name":"SHIP_AT225",
+	"modifications":[
+		{
+			"path":"CargoBaffles",
+			"property":"slot",
+			"recurse_to_variants":true,
+			"value":"cargo.modifierDivided"
+		}
+	]
+}
+
+const SHIP_OCP209 = {
+	"ship_name":"SHIP_OCP209",
+	"modifications":[
+		{
+			"path":"Bay/Bay/CargoBaffles",
+			"property":"slot",
+			"recurse_to_variants":true,
+			"value":"cargo.modifierAmorphic"
+		}
+	]
+}
+
+const SHIP_KITSUNE = {
+	"ship_name":"SHIP_KITSUNE",
+	"modifications":[
+		{
+			"path":"Front/CargoBaffles",
+			"property":"slot",
+			"recurse_to_variants":true,
+			"value":"cargo.modifierAmorphic"
+		}
+	]
+}
